65b16212059990d649850ea202e2e16ab43251af
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2020-10-09 10:09:05 +0100

    Clarified label

--- KpiAccelerator/KpiAccelerator/MainForm.cs
+++ KpiAccelerator/KpiAccelerator/MainForm.cs
@@ -80,7 +80,7 @@ namespace KpiAccelerator
 
         private void RefreshKPIs()
         {
-            this.LabelValueKpiLeadTime.Text = $"{Math.Round(this.KpiData.AverageLeadTime.TotalDays, 0)} days";
+            this.LabelValueKpiLeadTime.Text = $"{Math.Round(this.KpiData.AverageLeadTime.TotalDays, 0)} days Average Lead Time";
             this.LabelValueKpiDeployments.Text = $"{this.KpiData.NumberOfDeployments} in the last 3 months";
             this.LabelValueKpiSuccessful.Text = $"{this.KpiData.SuccessfulChangePercentage} % of changes in the last 3 months were successful";
             this.LabelValueKpiRecovery.Text = $"{Math.Round(this.KpiData.AverageRecoveryTime.TotalHours)} hours to recover from incidents";
